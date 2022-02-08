998f52531e8bdcc78e0ba6e048466abe605e1179
Asrbek <asrbekmahmudov>
2022-02-08 11:38:59 +0500

    Initial Commit

---
+++ Apelsin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apelsin
+//
+//  Created by Mahmudov Asrbek Ulug'bek o'g'li on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
